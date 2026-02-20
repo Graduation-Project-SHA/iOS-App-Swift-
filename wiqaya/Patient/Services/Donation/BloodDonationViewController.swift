4f1e64bce0cb946f619f55c577ad62cb72b15fda
Ahmad Alshafei <ahmadmarzoq44@gmail.com>
2026-02-20 20:08:54 +0200

    Work On Register Doctor

---
+++ wiqaya/Patient/Services/Donation/BloodDonationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BloodDonationViewController.swift
+//  wiqaya
+//
+//  Created by AhmadALshafei on 1/28/26.
+//
+
+import UIKit
+
+class BloodDonationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ wiqaya/Patient/Services/Donation/DonationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DonationTableViewCell.swift
+//  wiqaya
+//
+//  Created by AhmadALshafei on 1/25/26.
+//
+
+import UIKit
+
+class DonationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
